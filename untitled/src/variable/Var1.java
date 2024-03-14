2e3b618a0a3fd894b26496d3466da167ef34d92c
banibini <bani2115@gmail.com>
2024-03-14 20:14:54 +0900

    Initial commit

---
+++ untitled/src/variable/Var1.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(10); //변경 10 -> 20
+        System.out.println(10); //변경 10 -> 20
+        System.out.println(10); //변경 10 -> 20
+
+        /*
+        * 이럴경우 유지보수 및 동적이지 않아 힘들다.
+        * */
+
+    }
+}
---
+++ untitled/src/variable/Var4.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a; // 하나씩 선언할 수 있음
+        int b;
+
+        int c,d; // 한번에 여러 변수 선언 할수 있음
+
+
+    }
+}
